87bbf37880849a3e00ef87c5fd13fca0f2e5c367
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-24 08:45:57 -0300

    Adicionando comentarios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./config/app_config.js')
 
-
+// CONFIGURANDO ROTA MAIN DA APLICAÇÃO
 app.get('/', function(req, res){
     res.send('API Neri');
 })
--- config/app_config.js
+++ config/app_config.js
@@ -2,13 +2,16 @@ var express = require('express');
 var http = require('http');
 var app = module.exports = express();
 
-
+// DEFININDO PORTA PADRÃO
 app.set('port', process.env.PORT || 3000);
 
+// INSTANCIANDO SERVIDOR
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Servidor rodando na porta' + app.get('port'));
 });
 
+
+// MIDDLEWARE onde é setado no cabeçalho da Resposta o CORS e metodos suportados e outras configurações 
 app.use(function(req, res, next){
 	res.setHeader('Access-Control-Allow-Origin','*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PUT, DELETE');
